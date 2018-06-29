f4b0748c5ca1ec07aca110863bc4ec3ec157acbb
JFDValente <jfdv@icomp.ufam.edu.br>
2018-06-28 20:41:02 -0400

    alterada string de conexão com o banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/simbora');
+mongoose.connect('mongodb://JFDValente:Abrir123@ds121301.mlab.com:21301/simbora');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
